740ad8136f55173aa50c89ec1a1d5d24ae4b8fcc
Sajjad Ali <sajjadali.dev01@gamil.com>
2026-04-10 20:27:59 +0530

    feat: add Area of  rectangle value  calculator with input formatting  Question 5

---
+++ 5-Find-Area-of -Rectangle/area.py
@@ -0,0 +1,12 @@
+# WAP to find Area of Rectangle
+
+# Junior Level
+length = float(input("Enter value of Length : "))
+breadth = float(input("Enter value f Breadth : "))
+
+area = length * breadth  # Formula
+
+print(f'Area of Rectangle with Length={length:.2f} and Breadth={breadth:.2f} is {area:.2f}')
+
+
+# Senior Level
