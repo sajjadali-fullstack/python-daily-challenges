4aaf7a39d01d84018e0553fd58321c609229a00a
Sajjad Ali <sajjadali.dev01@gamil.com>
2026-04-11 21:25:55 +0530

    feat:  add two values in +, -, * HackerRank Problem Question 8

---
+++ 8-HackerRank-1/main.py
@@ -0,0 +1,33 @@
+# Task
+
+"""
+Add Provided code stub reads two integers drom STDIN, a & b.
+Add code to print 3 lines where :
+1. The first line contains the sum of the 2 numbers.
+2. The second line contains the difference of the 2 numbers
+(first-second).
+3. The third line contains the product of the 2 numbers.
+
+EG:~
+a = 3
+b = 5
+
+OP:~
+8
+-2
+15
+
+Explain :-
+3+5 --> 8
+3-5 --> -2
+3*5 --> 15
+"""
+
+
+
+a = int(input('Enter first Integer : '))
+b = int(input('Enter Second integer : '))
+
+print(a + b)
+print(a - b)
+print(a * b)
